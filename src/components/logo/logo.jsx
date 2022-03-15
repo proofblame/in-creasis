c949d233112d33dcaa1456f077e9639ada16bb74
Тимур Марокко <69273257+proofblame@users.noreply.github.com>
2022-03-15 21:16:38 +0300

    feat: create func navbar

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,9 +1,17 @@
 import style from './logo.module.scss'
 import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { closeMenu } from '../../services/actions/navbar'
 
 const Logo = () => {
+  const dispatch = useDispatch()
+
+  const handleCloseMenu = () => {
+    dispatch(closeMenu())
+  }
+
   return (
-    <Link className={style.link} to={'/'}>
+    <Link className={style.link} to={'/'} onClick={handleCloseMenu}>
       InCreasis
     </Link>
   )
